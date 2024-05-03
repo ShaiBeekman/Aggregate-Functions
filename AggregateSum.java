3d0d27f3bd4df66e9b053ad1fdf6d3ee4c0d5b43
Shai Beekman <shaibeekman@gmail.com>
2024-05-02 21:13:46 -0400

    Changed product.java to aggregateProduct.java; changed sum.java to aggregateSum.java; updated comments for accuracy; changed 'tuple_index' in aggregateProduct to 'index' to make reading it more simple

---
+++ AggregateSum.java
@@ -0,0 +1,20 @@
+import java.util.Scanner; //Scanner reads user input
+
+
+//sums all integers from 1 to n
+public class AggregateSum {
+    public static void main(String[] args) {
+        //input number
+        Scanner choice = new Scanner(System.in);
+        System.out.println("Choose n");
+        String n = choice.nextLine();
+        System.out.println("Choice of n is " + n);
+        int choicen = Integer.parseInt(n);
+
+        int sum = 1;
+        for (int index = 1; index <= choicen; index++) {
+            sum = sum + index;
+        }
+        System.out.println(sum);
+     }
+}
